cf0cc38d74c0c5e77a650d6e3b4962a2d1536849
Lucas Mazo <lucasmazo32@gmail.com>
2020-03-30 16:00:24 -0500

    finished first iteration, now wanting to create the app for mobile

---
+++ src/domChanges.js
@@ -0,0 +1,48 @@
+const cityName = document.querySelector('.city-name');
+const container = document.querySelector('.city-info');
+const imgCondition = document.querySelector('.condition');
+const tempUnits = document.querySelectorAll('.temp-unit');
+const minTemp = document.querySelector('.min-temp');
+const maxTemp = document.querySelector('.max-temp');
+const currTemp = document.querySelector('.curr-temp');
+const textCondition = document.querySelector('.text-condition');
+const humidity = document.querySelector('.humidity');
+
+const tempDom = (system) => {
+  if (system === 'metric') {
+    tempUnits.forEach((span) => {
+      span.innerHTML = '°C';
+    });
+  } else {
+    tempUnits.forEach((span) => {
+      span.innerHTML = '°F';
+    });
+  }
+};
+
+const unactiveToggle = (allElements) => {
+  allElements.forEach((element) => {
+    if (element.classList.contains('unactive')) {
+      element.classList.toggle('unactive');
+    } else {
+      element.classList.toggle('unactive');
+      window.setTimeout(() => {
+        element.classList.toggle('unactive');
+      }, 500);
+    }
+  });
+};
+
+const domChanges = (data, system) => {
+  cityName.innerHTML = `${data.name}, ${data.sys.country}`;
+  imgCondition.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  currTemp.innerHTML = data.main.temp;
+  minTemp.innerHTML = data.main.temp_min;
+  maxTemp.innerHTML = data.main.temp_max;
+  textCondition.innerHTML = data.weather[0].description;
+  humidity.innerHTML = data.main.humidity;
+  unactiveToggle([container]);
+  tempDom(system);
+};
+
+export default domChanges;
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,13 +1,4 @@
-// (`http://openweathermap.org/img/wn/${code}@2x.png`);
-const cityName = document.querySelector('.city-name');
-const imgCondition = document.querySelector('.condition');
-
-const domChanges = (data) => {
-  cityName.innerHTML = data.name;
-  cityName.classList.toggle('unactive');
-  imgCondition.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  imgCondition.classList.toggle('unactive');
-};
+import domChanges from './domChanges';
 
 const lookWeather = async (city, system) => {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=cbf4a4769da6bbf794164c449e463b35&units=${system}`);
@@ -15,7 +6,7 @@ const lookWeather = async (city, system) => {
   if (data.cod === '404') {
     console.log('Oops, seems like the city does not exist.');
   } else {
-    domChanges(data);
+    domChanges(data, system);
   }
 };
 
