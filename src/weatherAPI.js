40b0acc5bc64ba8376992f193a82483b1e0f21cd
Lucas Mazo <lucasmazo32@gmail.com>
2020-03-30 16:04:48 -0500

    solved lintern errors

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,11 +1,14 @@
 import domChanges from './domChanges';
 
+const noCity = document.querySelector('.no-city');
+
 const lookWeather = async (city, system) => {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=cbf4a4769da6bbf794164c449e463b35&units=${system}`);
   const data = await response.json();
   if (data.cod === '404') {
-    console.log('Oops, seems like the city does not exist.');
+    noCity.innerHTML = 'Oops, seems like the city does not exist.';
   } else {
+    noCity.innerHTML = '';
     domChanges(data, system);
   }
 };
