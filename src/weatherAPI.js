a28c0a87d1d8843d212586b32938e2705baae881
Lucas Mazo <lucasmazo32@gmail.com>
2020-03-30 12:57:41 -0500

    started to add to the DOM the necessary fields to use the given data from the weather api

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,7 +1,22 @@
+// (`http://openweathermap.org/img/wn/${code}@2x.png`);
+const cityName = document.querySelector('.city-name');
+const imgCondition = document.querySelector('.condition');
+
+const domChanges = (data) => {
+  cityName.innerHTML = data.name;
+  cityName.classList.toggle('unactive');
+  imgCondition.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  imgCondition.classList.toggle('unactive');
+};
+
 const lookWeather = async (city, system) => {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=cbf4a4769da6bbf794164c449e463b35&units=${system}`);
   const data = await response.json();
-  console.log(data);
+  if (data.cod === '404') {
+    console.log('Oops, seems like the city does not exist.');
+  } else {
+    domChanges(data);
+  }
 };
 
 export default lookWeather;
